d4503bd2bc7188bfe8f7e832f56cd7d7b9d1d3e2
bakufu <36562833+bakufu@users.noreply.github.com>
2022-11-17 07:58:43 +0200

    Update launch_and_del_wdb.bat
    Comments for .bat file

--- launch_and_del_wdb.bat
+++ launch_and_del_wdb.bat
@@ -1,4 +1,10 @@
+:: Made for WoW 1.12.1 client
+:: Simple bat file to delete WDB folder when you launch WoW
+:: Place this .bat file in your World of Warcraft folder and make shortcut for this.
 @echo off
+:: Edit your path as according below
 @RD /S /Q "X:\edit_your_path\World of Warcraft 1121\WDB\"
+:: Short timeout if your systems takes a second or two to delete the WDB file
 timeout /t 2
+:: Start WoW.exe
 start WoW.exe
